6e8b026467b1c072b46ef78b63a9fe74615a402f
hendraaagil <hendraaagil@gmail.com>
2024-12-05 20:52:08 +0700

    chore: add wiggle animation

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -56,6 +56,15 @@ const config: Config = {
         md: 'calc(var(--radius) - 2px)',
         sm: 'calc(var(--radius) - 4px)',
       },
+      keyframes: {
+        wiggle: {
+          '0%, 100%': { transform: 'rotate(-5deg)' },
+          '50%': { transform: 'rotate(5deg)' },
+        },
+      },
+      animation: {
+        wiggle: 'wiggle 2s ease-in-out infinite',
+      },
     },
   },
   plugins: [require('tailwindcss-animate')],
